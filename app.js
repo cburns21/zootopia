7ad2a0c26949a759086f1e61885597aa288f92f2
caroline burns <carolineburns@carolines-MacBook.local>
2019-02-15 13:26:17 -0700

    added routes

--- app.js
+++ app.js
@@ -2,10 +2,55 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
 const knex = require('./knex')
+var cors = require('cors')
+var bodyParser = require('body-parser');
+const dotenv = require('dotenv').config()
+
+app.use(cors())
+app.use(bodyParser.json());
 
 app.get('/', function(req, res, next){
-     res.send('bonjour bitches')
+    knex('animals')
+    .then((animals) => {
+        res.send(animals)
+    })
+    .catch((err) => {
+        next(err)
+    })
 })
+
+app.get('/cops', function(req, res, next){
+    knex('cops')
+    .then((cops) => {
+        res.send(cops)
+    })
+    .catch((err) => {
+        next(err)
+    })
+})
+
+app.get('/districts', function(req, res, next){
+    return knex('districts')
+        .join('cases', 'cases.district_id', 'districts.id')
+    .then((districts) => {
+        res.send(districts)
+    })
+    .catch((err) => {
+        next(err)
+    })
+})
+
+app.get('/', function(req, res, next){
+    knex('cases')
+    .then((cases) => {
+        res.send(cases)
+    })
+    .catch((err) => {
+        next(err)
+    })
+})
+
+
 
 app.listen(port, function(){
     console.log(`listening on port ${port}`)
